Fix wrong shape of mapping data

padding_dict_data.py
@@ -1,6 +1,5 @@
 from preprocessing_generate_data_collection import load_var, save_var
-
-
+import numpy as np
 
 
 def make_dict(data):
@@ -16,7 +15,35 @@ def make_dict(data):
             i += 1
 
     return data_dict
+
+
+def mapping_dict(data, data_dict):
+    mapping_list = []
+    for line in data:
+        new_line = []
+        for word in line.split(" "):
+            if word in data_dict:
+                new_line.append(data_dict[word])
+            else:
+                new_line.append(data_dict["<NULL>"])
+        mapping_list.append(new_line)
+
+    return np.array([np.array(line) for line in mapping_list])
+
+
+def padding_data(data, max_length):
+    new_data = []
+    for d in data:
+        line_len = len(d.split(" "))
+        if d.strip() == "":
+            d = (d + " <NULL>" * max_length).strip()
+        elif line_len < max_length:
+            d = (d + " <NULL>" * (max_length - line_len)).strip()
+        elif line_len > max_length:
+            d = " ".join(d.split(" ")[:max_length])
+        new_data.append(d)
     
+    return new_data
 
 
 if __name__ == '__main__':
@@ -45,8 +72,13 @@ if __name__ == '__main__':
     dict_codes = make_dict(codes)
     print(len(dict_codes))
     print(len(dict_descriptions))
-    save_var(dict_descriptions_path, dict_descriptions)
-    save_var(dict_codes_path, dict_codes)
+    pad_codes = padding_data(codes, 500)
+#    print(len(pad_codes[0]))
+#    print(pad_descriptions)
+    k = mapping_dict(pad_codes, dict_codes)
+    print(k.shape)
+#    save_var(dict_descriptions_path, dict_descriptions)
+#    save_var(dict_codes_path, dict_codes)
 
 #    print(descriptions[0])
 #    print("\n\n")

test_join.py
@@ -1,6 +1,10 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+
 l1 = ["1", "2", "3", "8"]
 l2 = ["r", "y"]
 #print(" ".join(l1))
 
 l1 += l2
-print(l1)
+print(a)